edb28de5d2369e2f0109ada94f9eceaa10e921e8
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2026-05-21 12:09:11 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let arr = new Array(26).fill(0)
+
+        // Flag letters with 1
+        for(const c of s){
+            arr[c.charCodeAt(0)-97]++
+        }
+
+        // Flag letters with -1
+        for(const c of t){
+            arr[c.charCodeAt(0)-97]--
+        }
+
+        let na = arr.filter((i) => i!=0);
+
+        return na.length == 0
+    }
+}
